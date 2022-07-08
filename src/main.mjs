f0f71f00f68983e30860aa2c2993e5e782b76490
Selwyn <talk@selwyn.cc>
2022-07-08 13:43:06 +0200

    wip

--- src/main.mjs
+++ src/main.mjs
@@ -3,33 +3,8 @@ import { getOctokit, context } from '@actions/github';
 
 async function main() {
 	const GITHUB_TOKEN = getInput('GITHUB_TOKEN');
-	const octokit = getOctokit(GITHUB_TOKEN)
-
-	const commentIdentifier = '<!---HACKATHONPRPREVIEWS-->'
-
-	const comment = commentIdentifier + `Hello commit`
-
-	const {data: comments} = await octokit.rest.issues.listComments({
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		issue_number: process.env.PR_NUMBER,
-	});
-	const myComment = comments.find(comment => comment.body.startsWith(commentIdentifier));
-	if (myComment) {
-		octokit.rest.issues.updateComment({
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			comment_id: myComment.id,
-			body: comment,
-		});
-	} else {
-		octokit.rest.issues.createComment({
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			issue_number: process.env.PR_NUMBER,
-			body: comment,
-		});
-	}
+	const octokit = getOctokit(GITHUB_TOKEN);
+	console.log({context})
 }
 
 main().catch(err => setFailed(err.message))
